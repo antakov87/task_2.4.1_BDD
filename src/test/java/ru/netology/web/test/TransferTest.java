599a787d4641e4c99fe7bbe5b96b75a5ec303847
Антаков Сергей <antakov87@mail.ru>
2022-11-04 12:59:52 +0400

    creating a project

---
+++ src/test/java/ru/netology/web/test/TransferTest.java
@@ -0,0 +1,76 @@
+package ru.netology.web.test;
+
+import com.codeborne.selenide.Configuration;
+import lombok.val;
+import org.junit.jupiter.api.Test;
+import ru.netology.web.data.DataHelper;
+import ru.netology.web.page.LoginPage;
+
+import static com.codeborne.selenide.Selenide.open;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class TransferTest {
+
+    @Test
+    void shouldTransferZeroCard() {
+        Configuration.holdBrowserOpen = true;
+        val loginPage = open("http://localhost:9999", LoginPage.class);
+        val authInfo = DataHelper.getAuthInfo();
+        val verificationPage = loginPage.validLogin(authInfo);
+        val verificationCode = DataHelper.getVerificationCodeFor(authInfo);
+        val dashboardPage = verificationPage.validVerify(verificationCode);
+        val balanceFirstCardBefore = dashboardPage.getFirstCardBalance();
+        val balanceSecondCardBefore = dashboardPage.getSecondCardBalance();
+        val transferPage = dashboardPage.secondCardDeposit();
+        int amount = 0;
+        transferPage.transferMoney(amount, DataHelper.getFirstCardNumber());
+        val balanceFirstCardAfter = dashboardPage.getFirstCardBalance();
+        val balanceSecondCardAfter = dashboardPage.getSecondCardBalance();
+        assertEquals((balanceFirstCardBefore - amount), balanceFirstCardAfter);
+        assertEquals((balanceSecondCardBefore + amount), balanceSecondCardAfter);
+    }
+
+    @Test
+    void shouldTransferCardFirstNumber() {
+        Configuration.holdBrowserOpen = true;
+        val loginPage = open("http://localhost:9999", LoginPage.class);
+        val authInfo = DataHelper.getAuthInfo();
+        val verificationPage = loginPage.validLogin(authInfo);
+        val verificationCode = DataHelper.getVerificationCodeFor(authInfo);
+        val dashboardPage = verificationPage.validVerify(verificationCode);
+        val balanceFirstCardBefore = dashboardPage.getSecondCardBalance();
+        val balanceSecondCardBefore = dashboardPage.getFirstCardBalance();
+        val transferPage = dashboardPage.firstCardDeposit();
+        int amount = 5350;
+        transferPage.transferMoney(amount, DataHelper.getSecondCardNumber());
+        val balanceSecondCardAfter = dashboardPage.getSecondCardBalance();
+        val balanceFirstCardAfter = dashboardPage.getFirstCardBalance();
+        assertEquals((balanceSecondCardBefore - amount), balanceSecondCardAfter);
+        assertEquals((balanceFirstCardBefore + amount), balanceFirstCardAfter);
+    }
+
+    @Test
+    void cancellationOfTheTransfer() {
+        val loginPage = open("http://localhost:9999", LoginPage.class);
+        val authInfo = DataHelper.getAuthInfo();
+        val verificationPage = loginPage.validLogin(authInfo);
+        val verificationCode = DataHelper.getVerificationCodeFor(authInfo);
+        val dashboardPage = verificationPage.validVerify(verificationCode);
+        val transferPage = dashboardPage.secondCardDeposit();
+        transferPage.transferCancel();
+    }
+
+//    @Test
+//    void shouldTransferMoreThanAllFromFirstCard() {
+//        val loginPage = open("http://localhost:9999", LoginPage.class);
+//        val authInfo = DataHelper.getAuthInfo();
+//        val verificationPage = loginPage.validLogin(authInfo);
+//        val verificationCode = DataHelper.getVerificationCodeFor(authInfo);
+//        val dashboardPage = verificationPage.validVerify(verificationCode);
+//        val transferPage = dashboardPage.secondCardDeposit();
+//        int amount = 24500;
+//        transferPage.transferMoney(amount, DataHelper.getFirstCardNumber());
+//        transferPage.failedTransfer();
+//    }
+
+}
